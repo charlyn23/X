55453fea0fe8246a030b53b25d7e691994b513a0
charlyn23 <charlyn.buchanan@gmail.com>
2015-03-18 23:55:45 -0400

    1st persist attempt

--- X/ViewController.swift
+++ X/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     var locationManager = CLLocationManager()
     var placemark = CLPlacemark()
     var dropPin = MKPointAnnotation()
-    var shameCoordinates: [Float]
+    var tapPoint: [CLLocation] = []
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,8 +40,10 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         var newShame = MKPointAnnotation()
-        println(returnPath())
-
+       // println(returnPath())
+       // var touchMapCoordinate
+        //self.shameCoordinates()[touchMapCoordinate]
+       // let shameCoordinates = NSArray(contentsOfFile: fileUrlString)
         
     }
     
@@ -86,30 +90,74 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
     //Dropping a pin
     func didLongTapMap(gestureRecognizer: UIGestureRecognizer) {
+        
         // Get the spot that was tapped.
         let tapPoint: CGPoint = gestureRecognizer.locationInView(mapView)
         let touchMapCoordinate: CLLocationCoordinate2D = mapView.convertPoint(tapPoint, toCoordinateFromView: mapView)
         println("pressed")
-        println(touchMapCoordinate)
         
         var newShame = MKPointAnnotation()
-        newShame.coordinate = touchMapCoordinate
+        var animatesDrop = true
         
         mapView.addAnnotation(newShame)
         mapView.selectAnnotation(newShame, animated: true)
+        newShame.coordinate = touchMapCoordinate
+        var shameLat = newShame.coordinate.latitude
+        var shameLong = newShame.coordinate.longitude
+        var shameCoordinate = (shameLat, shameLong)
+        println(newShame.coordinate.latitude)
+        println(newShame.coordinate.longitude)
+        
+    
+    let submitAlertController = UIAlertController(title: "New Shame", message: "Would you like to submit this shame?", preferredStyle: .Alert)
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+        // delete pin
+    }
+    submitAlertController.addAction(cancelAction)
+    
+    let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+        // store coordinates to plist
+        self.shameCoordinates.append(CLLocation(latitude: shameLat, longitude: shameLong))
+       //d self.returnPath()
+        
+    }
+    submitAlertController.addAction(OKAction)
+    
+    self.presentViewController(submitAlertController, animated: true) {
+    // ...
+    }
+    }
 
-}
+
     //Persist stuff
     func returnPath() -> NSURL {
         var directoryPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0] as NSURL
         return directoryPath.URLByAppendingPathComponent("ShamesLogged.plist")
         
     }
-    let filename = "ShamesLogged.plist"
-    var filePath = NSBundle.mainBundle().pathForResource("ShamesLogged", ofType: "plist")
-    let cocoaArray: NSArray = shameCoordinates[]
     
-    func storeShames() {
-        cocoaArray.writeToFile("///Users/charlynbuchanan/Library/Developer/CoreSimulator/Devices/624E3D48-A44B-46AC-BE0B-9125C4FBBE72/data/Containers/Data/Application/F1BBDDFE-6518-4328-A5C9-5C7FFA0F9519/Documents/ShamesLogged.plist", atomically: true)
+    var shameCoordinates: [CLLocation] = [shame Lat, shame Long] {
+        didSet {
+            var url = returnPath()
+            var atomic = true
+//            if let shameCoordinates = self.shameCoordinates
+                let result = (shameCoordinates as NSArray).writeToURL(url, atomically: atomic)
+                println(result)
+//            }
+        }
     }
+    let filename = "ShamesLogged.plist"
+    var UserDomaimnask = NSSearchPathDomainMask()
+//    var filePath: String {
+//        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, UserDomainmask, true)[0] as String
+//        return documentsDirectory.stringByAppendingPathComponent(filename)
+//    }
+
+    
+//    func storeShames() {
+//        cocoaArray.writeToFile("///Users/charlynbuchanan/Library/Developer/CoreSimulator/Devices/624E3D48-A44B-46AC-BE0B-9125C4FBBE72/data/Containers/Data/Application/F1BBDDFE-6518-4328-A5C9-5C7FFA0F9519/Documents/ShamesLogged.plist", atomically: true)
+//    }
+
 }
+
